ee862ca12f6bf1fa1b798e0e921c763a58f8e3c6
Navin <navinds25@gmail.com>
2021-03-22 08:49:41 +0530

    version 1

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+use actix_files as fs;
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
-async fn actix_main() -> std::io::Result<()> {
-    let address = String::from("0.0.0.0:5000");
+async fn main() -> std::io::Result<()> {
+    let address = String::from("192.168.1.1:5000");
     println!("starting server on address: {:?}", address);
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-    }).bind(address)?.run().await
-}
-
-fn main() {
-    println!("Hello, world!");
-    actix_main().unwrap();
+        App::new().service(fs::Files::new("/", ".").show_files_listing())
+    })
+    .bind(address)?
+    .run()
+    .await
 }
 
